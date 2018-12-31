e78095381479e84895dc05e069a81e66e851efd8
XinLiu92 <xliu0410@gmail.com>
2018-12-31 14:19:24 -0800

    add missing file

--- week1/oj-client/src/app/app.module.ts
+++ week1/oj-client/src/app/app.module.ts
@@ -4,17 +4,27 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ProblemListComponent } from './components/problem-list/problem-list.component';
+import { DataService } from './services/data.service';
+import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
+import { routing } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProblemListComponent,
+    ProblemDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [{
+    provide: 'data',
+    useClass: DataService
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ week1/oj-client/src/app/mock-problem.ts
@@ -0,0 +1,28 @@
+import { Problem } from './models/problem.model';
+export const PROBLEMS: Problem[] = [
+  {
+    id: 1,
+    name: 'two sum',
+    desc: 'do two sum',
+    difficulty : 'easy'
+  },
+  {
+    id: 2,
+    name: '3sum',
+    desc: 'do 3sum',
+    difficulty: 'medium'
+  }
+  ,
+  {
+    id: 3,
+    name: '4sum',
+    desc: 'do 4 sum',
+    difficulty: 'medium'
+  },
+  {
+    id: 4,
+    name: 'sliding wind',
+    desc: 'sl',
+    difficulty : 'hard'
+  }
+];
